0c5ca1284a29c4a7a7cb3813917984c72976aedc
Fabio Pieta <fabioxpieta@gmail.com>
2020-07-24 21:02:39 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class BoardException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	 public BoardException(String msg) {	// construtor que recebe a 'msg'	
+		 super(msg);						// repassando para o construtor da superclasse que é 'RuntimeException'
+	 }
+}
